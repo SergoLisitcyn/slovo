575f3d43c5f54bc60a9f35e85a92f27d8f72f633
Anatoliy Pechkin <anatmozg@mail.ru>
2021-12-08 13:56:25 +0300

    classname

--- frontend/views/pages/showPage.php
+++ frontend/views/pages/showPage.php
@@ -3,6 +3,6 @@ if(isset($model->title) && !empty($model->title)) { $this->title = $model->title
 if(isset($model->keywords) && !empty($model->keywords)) { $this->registerMetaTag(['name' => 'keywords','content' => $model->keywords]); }
 if(isset($model->description) && !empty($model->description)) { $this->registerMetaTag(['name' => 'description','content' => $model->description]); }
 ?>
-<div class="responsitive-text-page get-money-form text-page mobile-text-page">
+<div class="responsitive-text-page">
     <?php echo $model->content ?>
 </div>
